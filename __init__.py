0599dc1ef2d3f65b36a95c796797d59aedaab59f
Venkat Arun <venkatarun95@gmail.com>
2022-04-19 12:54:47 -0400

    Added Piecewise class

--- __init__.py
+++ __init__.py
@@ -1,2 +1,3 @@
 from .my_solver import MySolver, extract_vars
 from .utils import model_to_dict
+from .nonlinear import Piecewise
---
+++ nonlinear.py
@@ -0,0 +1,74 @@
+from my_solver import MySolver
+from typing import List, Optional, Tuple, Union
+import z3
+from z3 import And, ArithRef, BoolRef, If, Implies, Not, Sum
+
+
+class Piecewise:
+    '''
+    A variable that depends on a list of conditions. E.g. "x = 1 if y < 0 and x
+    = 2 otherwise", where x is the variable this class represents.
+
+    It is useful for defining multiplications for instance. The given
+    conditions *must* be mutually exclusive and exhaustive. This can be
+    verified through a separate Z3 query accessible through `verify`
+
+    '''
+
+    # To give unique names to piecewise variables
+    id: int = 0
+    # IF `val` has already been called
+    val_def: Optional[ArithRef]
+
+    def __init__(self, s: MySolver,
+                 vals: Union[List[Tuple[BoolRef, float]],
+                             List[Tuple[BoolRef, int]]]):
+        self.s = s
+        self.vals = vals
+        self.id = Piecewise.id
+        Piecewise.id += 1
+        # We may create multiple aux variables distinguished by this
+        self.aux_id = 0
+
+    def val(self) -> ArithRef:
+        aux = self.s.Real(f"aux_piecewise_{self.id},{self.aux_id}")
+        self.aux_id += 1
+        for (c, v) in self.vals:
+            self.s.add(Implies(c, aux == v))
+        return aux
+
+    def __mul__(self, other: Union[ArithRef, float, int]) -> ArithRef:
+        if self.val_def is not None:
+            return self.val_def
+        self.val_def = self.s.Real(f"aux_piecewise_{self.id},{self.aux_id}")
+        self.aux_id += 1
+        for (c, v) in self.vals:
+            self.s.add(Implies(c, self.val_def == v * other))
+        return self.val_def
+
+    def __add__(self, other: Union[ArithRef, float, int]) -> ArithRef:
+        aux = self.s.Real(f"aux_piecewise_{self.id},{self.aux_id}")
+        self.aux_id += 1
+        for (c, v) in self.vals:
+            self.s.add(Implies(c, aux == v + other))
+        return aux
+
+    def verify(self, s: Optional[MySolver] = None):
+        '''Verify that the conditions are mutually exclusive and exhaustive. Takes an
+        optional `MySolver` object that can contain additional constraints to
+        ensure this is the case.
+
+        '''
+        if s is None:
+            s = MySolver()
+
+        num_sat = Sum(*[If(c, 1, 0) for (c, _) in self.vals])
+        s.add(num_sat != 1)
+        s.check()
+        satisfiable = s.check()
+        assert satisfiable != z3.unsat, f"Unable to check that options are mutually exclusive and exhaustive. Got {satisfiable} for {str(self.vals)}"
+
+
+def create_linear_piecewise(start: float, end: float, step: float) -> Piecewise:
+    '''Construct a `Piecewise` object with linearly spaced pieces'''
+    pass
