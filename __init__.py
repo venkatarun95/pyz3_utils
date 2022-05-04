f7de2b2b06afa1d0898811d88ae3249932982216
Venkat Arun <venkatarun95@gmail.com>
2022-05-04 16:42:44 -0400

    Include binary_search into __init__

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
-from .my_solver import MySolver, extract_vars
+from .binary_search import BinarySearch
 from .cache import ModelDict, QueryResult, Variables, fill_obj_from_dict, model_to_dict, run_query
 from .cond import IfStmt
+from .my_solver import MySolver, extract_vars
 from .nonlinear import Piecewise
