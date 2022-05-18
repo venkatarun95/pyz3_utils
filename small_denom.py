a0c602a3e2a773980ec5769409b95c29bf9930b1
Venkat Arun <venkatarun95@gmail.com>
2022-05-18 15:32:33 -0400

    Better small_denom heuristic and richer API

--- small_denom.py
+++ small_denom.py
@@ -2,16 +2,21 @@
 
 from fractions import Fraction
 import math
-from typing import Optional
+from typing import Set, Optional
 from z3 import If, Or, Real
 
 from .binary_search import BinarySearch
 from .cache import ModelDict, model_to_dict
 from .my_solver import MySolver
 
-def find_small_denom_soln(s: MySolver, max_denom: int) -> Optional[ModelDict]:
+def find_small_denom_soln(s: MySolver,
+                          max_denom: int,
+                          target_vars: Optional[Set[str]] = None
+                          ) -> Optional[ModelDict]:
     '''Find a solution that tries to maximize the number of variables that have a
-    demoninator smaller than `max_denom`
+    demoninator smaller than `max_denom`. If target_vars is not None, focusses
+    only on making the given variables (specified by their name) have a small
+    denominator.
 
     '''
 
@@ -21,6 +26,9 @@ def find_small_denom_soln(s: MySolver, max_denom: int) -> Optional[ModelDict]:
 
     m = model_to_dict(s.model())
 
+    # Isolate the constraints this function adds from the outside.
+    s.push()
+
     best_m = m
     best_obj = 0
 
@@ -29,24 +37,28 @@ def find_small_denom_soln(s: MySolver, max_denom: int) -> Optional[ModelDict]:
     max_objective = 0
     old_obj = 0
     for vname in m:
+        if target_vars is not None and vname not in target_vars:
+            continue
         if type(m[vname]) is Fraction:
             val = m[vname]
+            # Fractions just above and below the value
+            num = math.ceil(Fraction(val.numerator * max_denom,
+                                     val.denominator))
+            hi = Fraction(num, max_denom)
+            lo = Fraction(num - 1, max_denom)
+            assert lo <= val and val <= hi, f"Error in computing hi={hi} and lo={lo} for {val}"
+
+            s.add(Or(
+                Real(vname) == val, Real(vname) == lo, Real(vname) == hi))
+
             if val.denominator > max_denom:
-                # Fractions just above and below the value
-                num = math.ceil(Fraction(val.numerator * max_denom,
-                                         val.denominator))
-                hi = Fraction(num, max_denom)
-                lo = Fraction(num - 1, max_denom)
-                assert lo < val and val < hi, f"Error in computing hi={hi} and lo={lo} for {val}"
-
-                s.add(Or(
-                    Real(vname) == val, Real(vname) == lo, Real(vname) == hi))
-
-                objective += If(Real(vname) == val, 0, 1)
                 max_objective += 1
+                objective += If(Real(vname) == val, 0, 1)
             else:
-                s.add(Real(vname) == val)
                 old_obj += 1
+        else:
+            if target_vars is not None:
+                print(f"Warning: {vname} present in `target_vars`, but its type is {type(m[vname])}, not Fraction")
 
 
     search = BinarySearch(0, max_objective, 1)
@@ -80,10 +92,13 @@ def find_small_denom_soln(s: MySolver, max_denom: int) -> Optional[ModelDict]:
 
     new_obj = 0
     for vname in best_m:
-        if type(best_m[vname]) is Fraction:
+        if (type(best_m[vname]) is Fraction and
+            (target_vars is None or vname in target_vars)):
             val = best_m[vname]
             if val.denominator <= max_denom:
                 new_obj += 1
     print(f"Improved number of small numbers from {old_obj} to {new_obj} out of a max of {old_obj + max_objective}")
 
+    # Remove all constraints we added
+    s.pop()
     return best_m
